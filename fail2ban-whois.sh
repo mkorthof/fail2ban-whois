7250589c38e86bb18c3fe8b789a23be7a2a6dff1
Marius <m-k@online.nl>
2017-03-13 23:51:27 +0100

    centos fix, uppercase country tld, remove cr+lf in short listing

--- fail2ban-whois.sh
+++ fail2ban-whois.sh
@@ -14,7 +14,7 @@ LIST=0
 if [ $( echo $* | grep '\-h' ) ]; then echo -e "\n$0 [-h] help\n$0 [-s] output short listing (default)\n$0 [-l] output long listing\n"; exit 0; fi
 if [ $( echo $* | grep '\-s' ) ]; then LIST=0; fi
 if [ $( echo $* | grep '\-l' ) ]; then LIST=1; fi
-for j in $( fail2ban-client status | grep "Jail list:" | cut -f 3- | sed 's/,//g' ); do 
+for j in $( fail2ban-client status | grep "Jail list:" | sed -e 's/.*:\t*\(.*\)/\1/g' -e 's/,//g' ); do 
   for ip in $( fail2ban-client status $j | grep "IP list:" ); do
     if [ "$( echo $ip | grep -E [0-9a-fA-F\.:]{4} )" ]; then
       echo -e "[JAIL] $j [IP] ${BOLD}$ip${SGR0} [WHOIS]\n$( whois $ip | \
@@ -23,10 +23,10 @@ for j in $( fail2ban-client status | grep "Jail list:" | cut -f 3- | sed 's/,//g
         sed -e 's/^ *//g' -e 's/  */ /g' | sed -e 's/ - /-/g' \
             -e 's/% Abuse contact for.*is '\''\(.*\)'\''/abuse-c: \1/g' -e "s/\(abuse-c:\|abuse-mailbox:\)/${SMUL}abuse:${RMUL}/g" \
             -e "s/\(inetnum:\|CIDR:\|IPv4 Address :\)/${SMUL}inetnum:${RMUL}/g" -e "s/\([nN]et[nN]ame:\|Service Name :\|ownerid:\)/${SMUL}netname:${RMUL}/g" \
-            -e "s/# KOREAN(UTF8)/${SMUL}country:${RMUL} ${BOLD}KR${SGR0}/" -e "s/[cC]ountry: \([a-zA-Z][a-zA-Z]\)\(.*\)/${SMUL}country:${RMUL} ${BOLD}\1${SGR0}\2/g" \
+            -e "s/# KOREAN(UTF8)/${SMUL}country:${RMUL} ${BOLD}KR${SGR0}/" -e "s/[cC]ountry: \([a-zA-Z][a-zA-Z]\)\(.*\)/${SMUL}country:${RMUL} ${BOLD}\U\1\E${SGR0}\2/g" \
             -e "s/\(Organization Name :\|owner:\)/${SMUL}descr:${RMUL}/g" | \
         sort -f | uniq -i | sed "s/descr_1:/${SMUL}descr:${RMUL}/g"
-      )" | ( [ $LIST -eq 0 ] && sed ':a;N;$!ba;s/\n/ /g' || cat )
+      )" | ( [ $LIST -eq 0 ] && sed ':a;N;$!ba;s/\(\n\|\r\n\)/ /g' || cat )
     fi
   done
 done
